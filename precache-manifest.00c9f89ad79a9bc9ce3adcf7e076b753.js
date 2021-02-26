d283bb11dc058139b76f31aeacbe58d38a1660de
Simply007 <9218736+Simply007@users.noreply.github.com>
2021-02-26 16:35:18 +0000

    Deploying to gh-pages from  @ 7617a4f2a9de405234e9c097008b5b1630d0a5d3 🚀

---
+++ precache-manifest.00c9f89ad79a9bc9ce3adcf7e076b753.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee88c753738b949d1c04",
+    "url": "/kontent-environments-comparer/css/app.00ceddf6.css"
+  },
+  {
+    "revision": "6ac986467391bb61b536",
+    "url": "/kontent-environments-comparer/css/chunk-vendors.ce65d627.css"
+  },
+  {
+    "revision": "63797d68c3d4dfe346ac47f52d59c145",
+    "url": "/kontent-environments-comparer/img/logo.63797d68.svg"
+  },
+  {
+    "revision": "7c129fcd993a298d079e7adbf9b02623",
+    "url": "/kontent-environments-comparer/index.html"
+  },
+  {
+    "revision": "ee88c753738b949d1c04",
+    "url": "/kontent-environments-comparer/js/app.a16feb09.js"
+  },
+  {
+    "revision": "6ac986467391bb61b536",
+    "url": "/kontent-environments-comparer/js/chunk-vendors.43f3834a.js"
+  },
+  {
+    "revision": "02a5f5cf9336ede7877483a00d0ced72",
+    "url": "/kontent-environments-comparer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kontent-environments-comparer/robots.txt"
+  }
+]);
